9664007a2c0574e42b168480b4f13c0e2e4744b1
Dima18au <dimor9diit@gmail.com>
2021-02-23 21:24:38 +0200

    Actions Test new

--- src/main/java/org/less6/les10/ActionsTest.java
+++ src/main/java/org/less6/les10/ActionsTest.java
@@ -1,2 +1,35 @@
-package org.less6.les10;public class ActionsTest {
+package org.less6.les10;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+public class ActionsTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\new\\chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+        System.out.println("start test");
+
+        String s = "C:\\Users\\Student\\IdeaProjects\\Dddv\\src\\main\\resources\\actions.html";
+
+        webDriver.get(s);
+        Actions actions = new Actions(webDriver);
+        try {
+            List<WebElement> li = webDriver.findElements(By.tagName("Li"));
+            actions.click(li.get(1))
+                    .keyDown(Keys.CONTROL)
+                    .moveToElement(li.get(5))
+                    .pause(2000)
+                    .click()
+                    .perform();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    webDriver.quit();
+    }
 }
